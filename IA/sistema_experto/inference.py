70febd889fc6910819af5b86548359c6106ee02d
Airam Armenta Vázquez <143845090+Airam21170247@users.noreply.github.com>
2026-03-18 18:01:08 +0000

    Actualizacion para Recomendacion X Impacto para Simulacion

--- IA/sistema_experto/inference.py
+++ IA/sistema_experto/inference.py
@@ -6,6 +6,7 @@ def infer_risk(facts):
     explanations_medical = []
     recommendations = []
     metrics = []
+    recomendaciones_con_impacto = []
 
     for rule in RULES:
         try:
@@ -16,7 +17,11 @@ def infer_risk(facts):
                 explanations_medical.append(rule["explication_medica"])
                 recommendations.append(rule["recommendation"])
 
-                # Copiamos la métrica y evaluamos si "value" es una función
+                recomendaciones_con_impacto.append({
+                    "recommendation": rule["recommendation"],
+                    "impact": rule["impact"]
+                })
+
                 metric = rule["metric"].copy()
                 if callable(metric["value"]):
                     metric["value"] = metric["value"](facts)
@@ -30,5 +35,6 @@ def infer_risk(facts):
         "explicacion_general": explanations_general,
         "explicacion_medica": explanations_medical,
         "recomendaciones": list(set(recommendations)),
+        "recomendaciones_con_impacto": recomendaciones_con_impacto,
         "metricas": metrics
     }
